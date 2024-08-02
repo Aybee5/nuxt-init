e74c45c10defcdbc9eb7fa64f8f15e6086de16a7
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2024-08-02 21:01:47 +0100

    adds debug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  debug:true,
   routeRules: {
     "/test/csr": { ssr: false },
     "/test/isr": { isr: 60 },
